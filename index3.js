ff60a7947de885fd8612ab21e1a47ac6ef858232
galbasha <georgesgithub@hotmail.com>
2023-11-24 22:59:11 -0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./stylesTictoe.css";
 
-import App from "./tictoe";
+import App from "./tictoe.js";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
